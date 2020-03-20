6e26131287034e9b9894258d0e0fdc1f7a8124ac
Muhammad Omar <muhammad.omar.eg@gmail.com>
2020-03-21 01:59:25 +0200

    add circle ci badge and update readme file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,16 +3,22 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# This is your Docker ID
+#dockerpath=""
+dockerpath="omaroovee/ml-service"
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+# Run a Docker Hub container with kubernetes
+kubectl run ml-service-demo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-service-demo
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-
+# Forward the container port to host
+kubectl port-forward ml-service-demo 8000:80

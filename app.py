76849bf0b1cc604f0476458a5e0c577183204217
Muhammad Omar <muhammad.omar.eg@gmail.com>
2020-03-21 02:11:46 +0200

    add circle ci badge and update readme file

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    
-    LOG.info(f"Scaling Payload: \n{payload}")
+
+    LOG.info('Scaling Payload: %s',payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -26,43 +26,41 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     """Performs an sklearn prediction
-        
-        input looks like:
-        {
-        "CHAS":{
-        "0":0
-        },
-        "RM":{
-        "0":6.575
-        },
-        "TAX":{
-        "0":296.0
-        },
-        "PTRATIO":{
-        "0":15.3
-        },
-        "B":{
-        "0":396.9
-        },
-        "LSTAT":{
-        "0":4.98
-        }
-        
-        result looks like:
-        { "prediction": [ <val> ] }
-        
-        """
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+    result looks like:
+    { "prediction": [ <val> ] }
+    """
+
     # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
+    LOG.info('JSON payload: %s',json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    LOG.info('Inference payload DataFrame: %s', inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"output prediction: \n{prediction}")
+    LOG.info('Output prediction: %s',prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
